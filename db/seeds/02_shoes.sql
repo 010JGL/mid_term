e283acb520b6b9a9b7d320ee6835696660c2799e
Jeremy <010JGL@GMAIL.COM>
2023-04-08 20:22:44 -0400

    Add link to pictures

--- db/seeds/02_shoes.sql
+++ db/seeds/02_shoes.sql
@@ -1,4 +1,6 @@
 INSERT INTO shoes (gender, price, brand, size, image_url, is_sold, description)
-VALUES ('male', '10000', 'Adidas', '10', 'url', 'false', 'Brand new, out of the box, white Adidas shoes.'),
-('male', '12000', 'Adidas', '9', 'url', 'false', 'Never used before, perfect condition!'),
-('male', '50000', 'Air jordan', '12', 'url', 'false', 'Mint condition. Special edition');
+VALUES ('male', '10000', 'Adidas', '10', 'docs\pics\adidas 1.jpg', 'false', 'Brand new, out of the box, white Adidas shoes.'),
+('male', '12000', 'Adidas', '9', 'docs\pics\adidas 2.jpg', 'false', 'Never used before, perfect condition!'),
+('male', '50000', 'Air jordan', '12', 'docs\pics\air jordan 3.jpg', 'false', 'Mint condition. Special edition');
+
+-- not sure how to link images
