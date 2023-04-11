be63ec361a7d6da9c9728955813011e18338607d
Jeremy <010JGL@GMAIL.COM>
2023-04-11 16:47:56 -0400

    added form on listings_new.ejs

--- routes/listings.js
+++ routes/listings.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { addListing } = require('../db/queries/shoes');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,13 +9,23 @@ router.get('/', (req, res) => {
 //will need if statements to make sure user is loggedin in and has admin priveleges for these two routes
 
 router.get('/new', (req, res) => {
+
   res.render('listings_new');
 });
 
 
+
 router.post('/new', (req, res) => {
   //redirect to listing upon submission
-  res.redirect('/:id');
+  const userId = req.user_id;
+  console.log('req.body', req.body);
+  console.log('req.params', req.params);
+  if (!userId) {
+    const box = `<div>You have to be logged in</div>`;
+    return res.status(400).send(box);
+  }
+  addListing(userId)
+  res.redirect('/my_listings');
 });
 
 router.get('/:id', (req, res) => {
