f686f10d1b24782e645060592f777f0e5b3b0725
Jeremy <010JGL@GMAIL.COM>
2023-04-13 16:47:49 -0400

    add functions to write message, change to ejs files for the routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,17 +1,17 @@
 
 const express = require('express');
-const { showMessagesForListing, showMessagesForItem } = require('../db/queries/messages');
+const { showMessagesForItem, writeMessage, getSellerIdwithShoeId, } = require('../db/queries/messages');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
   const currentUser = req.session.userId;
-  console.log('currentUser',currentUser);
+  //console.log('currentUser',currentUser);
 
   showMessagesForItem(currentUser)
 
   .then(result => {
     const templateVars = { result }
-    console.log('templateVars', templateVars)
+    //console.log('templateVars', templateVars)
     res.render('messages', templateVars);
   })
 
@@ -25,11 +25,23 @@ router.get('/new', (req, res) => {
 //redirect to listing of item related to message or back to messages?
 router.post('/new', (req, res) => {
   const currentUser = req.session.userId;
+  //console.log('req.body', req.body)
+  const newMessage = req.body['new-message'];
+  const shoeId = req.body['shoe-id'];
 
+  getSellerIdwithShoeId(shoeId)
+  .then((result) => {
+    const receiverId = result.seller_id;
+    writeMessage(shoeId, newMessage, currentUser, receiverId)
+    .then(() => {
+      res.redirect('/messages')
+    })
+  })
+  .catch((err) => {
+    console.log('Cant write the new mesage;', err.message);
+    return null;
+  });
 
-  writeMessage(shoeId, currentUser, receiverId)
-
-  res.redirect('messages')
 });
 
 module.exports = router;
