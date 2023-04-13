ac257c9ed5380dc0aafef3f2728a88dc64aab891
Jeremy <010JGL@GMAIL.COM>
2023-04-12 22:45:06 -0400

    Add the messages pages, with the routes and DB link

--- db/queries/messages.js
+++ db/queries/messages.js
@@ -24,28 +24,47 @@ const writeMessage = (shoeId, senderId, receiverId) => {
 // show the messages (should show messages associated with the shoe_id / dont just show all the messages in one place)
 // When you click my listings, you can see your items. If you click on an item you should see messages associated with it
 //SHOULD BE CAHNGE FOR SPECIFIC ID
-const showMessagesForListing = (userId, shoeId) => {
+
+///////////// NOT USING THIS ONE RIGHT NOW //////////////
+// const showMessagesForListing = (userId, shoeId) => {
+//   const currentUser = userId;
+//   //const currentShoes = shoeId;   NOT USING IT FOR NOW
+//   return pool
+//     .query(`SELECT role FROM users WHERE users.id = $1;`, [currentUser])
+//     .then((result) => {
+//       console.log('result:', result);
+//       if (result.rows === 'admin') {
+//         // show admin/seller side logic
+//         pool
+//           .query(`SELECT shoes.id, messages.message, messages.date, users.name FROM messages JOIN shoes ON shoe_id = shoes.id JOIN users ON sender_id = users.id WHERE users.id = $1 OR receiver_id = $1 ORDER BY date;`, [currentUser]);
+
+//       } else {
+//         // show user/buyer side logic
+//         pool
+//           .query(`SELECT shoes.id, messages.message, messages.date, users.name FROM messages JOIN shoes ON shoe_id = shoes.id JOIN users ON receiver_id = users.id WHERE users.id = $1 OR sender_id = $1 ORDER BY date;`, [currentUser]);
+//       }
+//     })
+//     .catch((err) => {
+//       console.log('add user error;', err.message);
+//       return null;
+//     });
+
+// };
+// maybe we can modify to include the shoe id in reference
+// Add a scss file for the display here
+// we could show the item up top in a box
+const showMessagesForItem = (userId, shoeId) => {
   const currentUser = userId;
-  //const currentShoes = shoeId;   NOT USING IT FOR NOW
   return pool
-    .query(`SELECT role FROM users WHERE users.id = $1;`, [currentUser])
+    .query(`SELECT shoes.id, messages.message, messages.date, users.name FROM messages JOIN shoes ON shoe_id = shoes.id JOIN users ON sender_id = users.id WHERE users.id = $1 OR receiver_id = $1 ORDER BY date;`, [currentUser])
     .then((result) => {
       console.log('result:', result);
-      if (result.rows === 'admin') {
-        // show admin/seller side logic
-        pool
-          .query(`SELECT shoes.id, messages.message, messages.date, users.name FROM messages JOIN shoes ON shoe_id = shoes.id JOIN users ON sender_id = users.id WHERE users.id = $1 OR receiver_id = $1 ORDER BY date;`, [currentUser]);
-      } else {
-        // show user/buyer side logic
-        pool
-          .query(`SELECT shoes.id, messages.message, messages.date, users.name FROM messages JOIN shoes ON shoe_id = shoes.id JOIN users ON receiver_id = users.id WHERE users.id = $1 OR sender_id = $1 ORDER BY date;`, [currentUser]);
-      }
+      return result.rows;
     })
     .catch((err) => {
       console.log('add user error;', err.message);
       return null;
     });
-
 };
 
-module.exports = { writeMessage, showMessagesForListing }
+module.exports = { writeMessage, showMessagesForListing, showMessagesForItem }
--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,20 @@
+
 const express = require('express');
+const { showMessagesForListing, showMessagesForItem } = require('../db/queries/messages');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('messages');
+  const currentUser = req.session.userId;
+  console.log('currentUser',currentUser);
+
+  showMessagesForItem(currentUser)
+
+  .then(result => {
+    const templateVars = { result }
+    console.log('templateVars', templateVars)
+    res.render('messages', templateVars);
+  })
+
 });
 
 router.get('/new', (req, res) => {
