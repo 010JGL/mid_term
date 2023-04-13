ac894858e778b084fee1c879770a6cd467a1f23a
Jeremy <010JGL@GMAIL.COM>
2023-04-13 12:25:08 -0400

    add css for messages page, changed messages seeds

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,9 +1,9 @@
 INSERT INTO messages (shoe_id, message, date, sender_id, receiver_id)
 VALUES ('3', 'We need to input message here', '2023-01-19 03:14:07', '4', '2'),
-('3', 'Here is some pre recorded message for testing', '2023-02-01 01:24:22', '2', '4'),
-('3', 'Here is some pre recorded message for testing2', '2023-02-12 06:04:11', '4', '2'),
-('3', 'Here is some pre recorded message for testing3', '2023-03-13 03:34:09', '2', '4'),
-('3', 'Here is some pre recorded message for testing5', '2023-03-14 13:12:07', '4', '2'),
-('3', 'Here is some pre recorded message for testing6', '2023-03-18 23:14:06', '2', '4');
+('3', 'Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message!', '2023-02-01 01:24:22', '2', '4'),
+('3', 'Here is some pre recorded message for testing2 , this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message!', '2023-02-12 06:04:11', '4', '2'),
+('3', 'Here is some pre recorded message for testing3 , this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message!', '2023-03-13 03:34:09', '2', '4'),
+('3', 'Here is some pre recorded message for testing5 , this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message!', '2023-03-14 13:12:07', '4', '2'),
+('3', 'Here is some pre recorded message for testing6 , this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message! Here is some pre recorded message for testing, this is a very long message!', '2023-03-18 23:14:06', '2', '4');
 
 
