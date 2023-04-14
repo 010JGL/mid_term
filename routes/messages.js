34fab08570e7784c0c359c9553e148a0abeaf149
Jeremy <010JGL@GMAIL.COM>
2023-04-13 22:54:47 -0400

    email feature

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const { showMessagesForItem, writeMessage, getSellerIdwithShoeId, } = require('../db/queries/messages');
 const router  = express.Router();
+const nodemailer = require('nodemailer');
+const { findEmailById } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   const currentUser = req.session.userId;
@@ -23,11 +25,45 @@ router.get('/new', (req, res) => {
   res.render('new_message', templateVars);
 });
 
-// router.get('/messages', (req, res) => {
+router.post('/email', (req, res) => {
 
-//     res.redirect('/');
+  // how to send an email  for gmail
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'sneakervaultlhl@gmail.com',
+      pass: 'rogdwnenbagsptpn'
+    }
+  });
+  const currentUser = req.session.userId;
+  const currentSubject = req.body['your_subject'];
+  const currentMessage = req.body['new-email']
 
-// });
+  findEmailById(currentUser)
+
+    .then((result) => {
+      const currentUserEmail = result.email;
+      //console.log('currentUserEmail', currentUserEmail)
+      const mailOptions = {
+        from: 'SneakerVaultLHL@gmail.com',
+        to: currentUserEmail,
+        subject: currentSubject,
+        text: currentMessage
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.redirect('/messages')
+        }
+      })
+
+    })
+
+
+});
 
 
 //redirect to listing of item related to message or back to messages?
