b7dc98c44ec99439fa8c34b5edaf2a2199d656ad
hanvidlee <hanvidlee@gmail.com>
2023-04-14 09:30:55 -0400

    added styling to favourites

--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -1,6 +1,9 @@
 INSERT INTO favorites (user_id, shoes_id)
+VALUES ('6', '2');
+
+INSERT INTO favorites (user_id, shoes_id)
 VALUES ('6', '3'),
--- ('2', '4'),
+('2', '4'),
 ('2', '5'),
 ('2', '6'),
 ('2', '7'),
